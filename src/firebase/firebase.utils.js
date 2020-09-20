fc47c0a5ffd3bfa07b46ed35a16a04f743c881e4
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2020-09-20 20:51:21 +0800

    Added redux, and implemented userReducer and userActions to header and app components

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: 'AIzaSyA1Yux8qmwju63-VMX4WqHCrsOp8vRmxMg',
+  authDomain: 'crwn-clothing-3-db.firebaseapp.com',
+  databaseURL: 'https://crwn-clothing-3-db.firebaseio.com',
+  projectId: 'crwn-clothing-3-db',
+  storageBucket: 'crwn-clothing-3-db.appspot.com',
+  messagingSenderId: '834478342925',
+  appId: '1:834478342925:web:5507080b2b5f003e002644',
+  measurementId: 'G-607EL5QDMN'
 };
 
 firebase.initializeApp(config);
